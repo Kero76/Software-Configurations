38bd3ced1a361d2343e1ac4fdf781b2af07d1f98
Nicolas GILLE <nic.gille@gmail.com>
2019-12-31 12:55:31 +0100

    [Feat] - Add extension.sh file to install automatically all needed extensions.

---
+++ vscode/extensions.sh
@@ -0,0 +1,5 @@
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension mdickin.markdown-shortcuts
+code --install-extension alanwalk.markdown-toc
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension eg2.vscode-npm-script
