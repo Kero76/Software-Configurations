1b90785ef23080f49611b6e82d0e7dac5215c52b
Nicolas GILLE <nic.gille@gmail.com>
2020-01-05 18:50:28 +0100

    [Feat] - Add new extension for vscode.

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -3,3 +3,5 @@ code --install-extension mdickin.markdown-shortcuts
 code --install-extension alanwalk.markdown-toc
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension eg2.vscode-npm-script
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension esbenp.prettier-vscode
