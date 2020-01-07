dbc363cf346c84ca6df841c06cd90c8d9b23f2c3
Nicolas GILLE <nic.gille@gmail.com>
2020-01-07 10:48:05 +0100

    [Feat] - Update extensions.sh

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,11 +1,29 @@
+### Script used to install vscode extensions.
+### 
+### @author Nicolas GILLE <nic.gille@gmail.com>
+### @license MIT
+### @Version 0.4
+
+# About source code
+code --install-extension esbenp.prettier-vscode
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension chrmarti.regex
+
+# Improve vscode functionality
+code --install-extension gruntfuggly.todo-tree
+code --install-extension alefragnani.bookmarks
+code --install-extension formulahendry.terminal
+
+# Git
+code --install-extension eamodio.gitlens
+
+# Languages
+## Markdown
 code --install-extension shd101wyy.markdown-preview-enhanced
 code --install-extension mdickin.markdown-shortcuts
 code --install-extension alanwalk.markdown-toc
 code --install-extension davidanson.vscode-markdownlint
+
+## JavaScript and cie 
 code --install-extension eg2.vscode-npm-script
-code --install-extension coenraads.bracket-pair-colorizer-2
-code --install-extension esbenp.prettier-vscode
-code --install-extension chrmarti.regex
-code --install-extension alefragnani.bookmarks
-code --install-extension gruntfuggly.todo-tree
-code --install-extension streetsidesoftware.code-spell-checker
